309827f601d340865ba7e00f68865bb3366b96c0
visualstudio <visualstudio@122-PC11>
2019-02-08 21:01:17 -0500

    SOHIT

--- CSD3354-1-WEEK05/Program.cs
+++ CSD3354-1-WEEK05/Program.cs
@@ -6,10 +6,86 @@ using System.Threading.Tasks;
 
 namespace CSD3354_1_WEEK05
 {
+
     class Program
     {
         static void Main(string[] args)
         {
+            Elevator e = new Elevator();
+            e.setup();
+            e.TraverseList();
+        }
+    }
+
+    class Node
+    {
+        public Node() { }
+        public Node elevatorUp;
+        public string FloorNumber;
+    }
+
+    class Department
+    {
+        public Department(string dept_name)
+        {
+            DepartmentDescription = dept_name;
+        }
+        public Department aDepartment;
+        public string DepartmentDescription;
+    }
+
+    class DepartmentStore
+    {
+        public void InitializeDepartments()
+        {
+            Department Kitchenware = new Department("Kitchenware");
+            Department Books = new Department("Books");
+
+        }
+
+    }
+
+    class Elevator
+    {
+        Node Head;
+        Node FirstFloor;
+        Node SecondFloor;
+        Node ThirdFloor;
+        Node FourthFloor;
+
+        public void setup()
+        {
+            FirstFloor = new Node();
+            SecondFloor = new Node();
+            ThirdFloor = new Node();
+            FourthFloor = new Node();
+            Head = FirstFloor;
+            FirstFloor.FloorNumber = "First Floor";
+            FirstFloor.elevatorUp = SecondFloor;
+            SecondFloor.FloorNumber = "Second Floor";
+            SecondFloor.elevatorUp = ThirdFloor;
+            ThirdFloor.FloorNumber = "Third Floor";
+            ThirdFloor.elevatorUp = FourthFloor;
+            FourthFloor.FloorNumber = "Fourth Floor";
+            FourthFloor.elevatorUp = null;
+        }
+
+        public void TraverseList()
+        {
+            Node temp;
+            temp = Head;
+
+            // where am I going to start?
+
+            while (temp != null)
+            {
+                Console.WriteLine(temp.FloorNumber);
+                temp = temp.elevatorUp;
+
+            }
+
         }
     }
 }
+
+    
